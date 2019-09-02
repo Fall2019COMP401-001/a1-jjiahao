9e814d57e6e800878b9bc6ce2b72b589452317e3
sean <sean@shaojiahaodembp.wireless-1x.unc.edu>
2019-09-02 01:18:52 -0400

    1

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -26,35 +26,41 @@ public class A1Jedi {
 		}
 		// Read in count of numbers to process
 		int countCustomer = scan.nextInt();
-		int[] totalNumber = new int[count];
-		int[] total = new int[count];
+		int[] totalItemBought = new int[count];
+		int[] numberOfCustomerBought = new int[count];
 		
 		for (int i=0; i<countCustomer; i++) {
 			// Read in count of strings to process
 			String firstName = scan.next();
 			String lastName = scan.next();
 			int countItem = scan.nextInt();
+			int[] boughtOrNot = new int[count];
 			// Create an array to store numbers.
-			int[] countQuantity = new int[countItem];
-			String[] itemName2 = new String[countItem];
-			int countNumber = 0;
-			int countTotal = 0;
 			for (int j=0; j<countItem; j++) {
-				countQuantity[j] = scan.nextInt();
-				itemName2[j] = scan.next();
-				if (itemName2[j].equals(itemName[i])) {
-					countNumber++;
-					countTotal += countQuantity[j];
+				int numberOfItem = scan.nextInt();
+				String itemName2 = scan.next();
+				
+				for (int k=0; k<count; k++) {
+					int temp = 0;
+					if (itemName[k].equals(itemName2)) {
+						temp = numberOfItem;
+						totalItemBought[k] += temp;
+						boughtOrNot[k] = 1;
+					} 
 				}
 			}
-		totalNumber[i] = countNumber;
-		total[i] = countTotal;
+			
+			for (int l=0; l<count; l++) {
+				if (boughtOrNot[l] == 1) {
+					numberOfCustomerBought[l]++;
+				}
+			}
 		}
 		for (int i=0; i<count; i++) {
-			if (totalNumber[i] == 0) {
+			if (numberOfCustomerBought[i] == 0) {
 				System.out.println("No customers bought " + itemName[i]);
 			} else {
-				System.out.println( total[i] + " customers bought " + totalNumber[i] + " " + itemName[i]);
+				System.out.println(numberOfCustomerBought[i] + " customers bought " + totalItemBought[i] + " " + itemName[i]);
 			}
 		}
 	}
